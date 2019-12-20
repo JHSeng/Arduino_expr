1f0a0bedfffd3a89e910214643641cebdeb44c81
JHSeng <zxc98567@gmail.com>
2019-12-20 20:37:20 +0800

    update esp8266

--- ESP8266_softwareserial/ESP8266_softwareserial.ino
+++ ESP8266_softwareserial/ESP8266_softwareserial.ino
@@ -3,16 +3,13 @@ SoftwareSerial mySerial(3, 2);  // RX, TX
 
 void setup() {
     Serial.begin(9600);
-    while (!Serial) {;}
-    Serial.println("hardware serial start!");
     mySerial.begin(9600);
-    mySerial.println("software serial start!");
+    mySerial.println("Hello! ESP8226 start working!");
 }
 
 void loop() {
     if (mySerial.available())
         Serial.write(mySerial.read());
-
     if (Serial.available())
         mySerial.write(Serial.read());
 }
