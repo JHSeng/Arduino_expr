5024f69f3e34c42d4ef5a67bf26dadb026ed239d
JHSeng <zxc98567@gmail.com>
2019-12-21 17:00:16 +0800

    update esp8266

---
+++ ESP8266_softwareserial/ESP8266.ino
@@ -0,0 +1,23 @@
+#include <ESP8266WiFi.h>
+
+#define MAX_SRV_CLIENTS 4
+
+const char *ssid = "raspbirrypi4b";
+const char *password = "";
+IPAddress localIP(10, 3, 141, 199);
+IPAddress gateway(10, 3, 141, 1);
+IPAddress subnet(255, 255, 255, 0);
+
+void connectWiFi() {
+    WiFi.config(localIP, gateway, subnet);
+    WiFi.mode()
+}
+
+void setup() {
+    Serial.begin(9600);
+
+}
+
+void loop() {
+
+}
